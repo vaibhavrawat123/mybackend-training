2ccfa3d54097be7f815c127ccbe5b0d30ab4320c
Vaibhav Rawat <vaibhavkundra0533@gmail.com>
2022-09-03 00:27:22 -0700

    hum

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,4 +1,121 @@
 let axios = require("axios")
+//--------------------problem1-----------------------------------
+let getByDistrict = async function (req, res) {
+    try {
+        // let id = req.params.districtId
+        let district = req.query.districtId
+        let date = req.query.date
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}
+            &date=${date}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+//----------------------------problem2--------------------------------
+
+let getSortedCities = async function (req, res){
+
+    try {
+        let cities = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"];
+        let arr = [ ]
+        for (i = 0; i <= cities.length; i++) {
+            let object = { city: cities[i] }
+            let result = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}
+            &appid=5ec34cade1bed34af357df229a70f67d`)
+            console.log(result.data.main.temp)
+            object.temp = result.data.main.temp
+            arr.push (object)
+        }
+        let sorted = arr.sort( function (x, y) {
+            return x.temp - y.temp
+        })
+        console.log(sorted)
+        res.status(200).send({ status: true, data: sorted })
+
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ status: false, msg: "server error" })
+    }
+}
+
+//--------------------------problem 3---------------------------------
+
+
+let getMemes = async function (req, res) {
+    try {
+        let options = {
+            method: 'get',
+            url: `https://api.imgflip.com/get_memes`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+const myMeme = async function (req, res) {
+    try {
+        let template_id = req.query.template_id
+        let text0 = req.query.text0
+        let text1 = req.query.text1
+        
+        let username = req.query.username
+         let password = req.query.password
+        console.log(`query params are: ${template_id} ${text0} ${text1}${text1}${username} ${password}`)
+        var options = {
+            method: "post",
+            url : `https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}
+            &text1=${text1}&username=${username}&password=${password}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+module.exports.getMemes = getMemes; 
+module.exports.myMeme = myMeme;
+module.exports.getByDistrict = getByDistrict
+module.exports.getSortedCities = getSortedCities
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 let getStates = async function (req, res) {
@@ -60,7 +177,7 @@ let getByPin = async function (req, res) {
 let getOtp = async function (req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
--- src/routes/route.js
+++ src/routes/route.js
@@ -14,7 +14,10 @@ router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
-
+router.get("/cowin/getByDistrictId", CowinController.getByDistrict)
+router.get("/weather/getSortedCities", CowinController.getSortedCities)
+router.get("/Meme", CowinController.getMemes)
+ router.post("/myMeme", CowinController.myMeme)
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
 
